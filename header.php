fb3f523208ed6f4bd848519ac53bd4cd01e381a7
akrigline <andrew@akrigline.com>
2015-01-31 16:51:43 -0500

    get post from about category for pitch

--- header.php
+++ header.php
@@ -53,7 +53,7 @@
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
             <![endif]-->
-            <?php if ( is_page('Home') ) { ?>
+            <?php if ( is_page('home') ) { ?>
             <!-- STANDARD NAVIGATION -->
             <nav class="nav">
                 <a href="index.html"><div class="logo"></div></a>
