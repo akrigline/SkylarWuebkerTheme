9ad46e7ff9eb881524f6a98e3df7986854b2e2eb
akrigline <andrew@akrigline.com>
2015-01-31 15:59:33 -0500

    First commit of redo

--- header.php
+++ header.php
@@ -6,115 +6,78 @@
  *
  * @package SkylarWuebker2
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
+?>
 
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"<?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php bloginfo('title'); ?></title>
-    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/jquery.fancybox.css" media="screen">
-    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/jquery.sidr.light.css" media="screen">
-    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/jquery.fancybox.css" media="screen" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/jquery.sidr.light.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/main.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Quicksand:400,700|Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Arimo:400,700|Numans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
     <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <style>
+        #button {
+            display: none;
+        }
+        @media only screen and (max-width: 767px){
+            #button {
+                display: block;
+            }
+            .nav {
+                display:none;
+            }
+        }
 
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-<?php wp_head(); ?>
-
+        #mobile-nav{
+            display:none;
+        }
+    </style>
+    <?php wp_head(); ?>
 </head>
+<body<?php body_class(); ?>>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+            <![endif]-->
+            <?php if ( is_page('Home') ) { ?>
+            <!-- STANDARD NAVIGATION -->
+            <nav class="nav">
+                <a href="index.html"><div class="logo"></div></a>
+                <div class="clear"></div>
+                <ul class="filters">
+                    <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Work'))); ?>">WORK</a></li>
+                    <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Contact'))); ?>">CONTACT</a></li>
+                    <li><a target=blank href="<?php bloginfo('template_directory');?>/photos/SkylarResume.pdf">RESUME</a></li>
+                </ul>
+                <div class="clear"></div>
+            </nav>
 
-<body <?php body_class(); ?>>
-<!--[if lt IE 7]>
-    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
+            <!-- MOBILE NAVIGATION TRIGGER -->
+            <aside id="button" class="sidebar-small pull-right">
+                <a id="menu-btn" href="#mobile-nav"><img src="photos/button-white.png"></a>
+            </aside>
 
-
-<?php if ( is_page('Home') ) { ?>
-    <!-- STANDARD NAVIGATION -->
-    <nav id="nav" class="col-sm-3 hidden-xs">
-        <div class="logo"><a href="#"><img src="<?php bloginfo('template_directory');?>/photos/logo.png"></a></div>
-        <ul class="filters">
-            <li><a class="filter" href="#" data-filter="*">HOME</a></li>
-            <li><a class="filter" href="#identity" data-filter=".identity">IDENTITY</li></a> 
-            <li><a class="filter" href="#web" data-filter=".web">WEB</a></li>
-            <li><a class="filter" href="#print" data-filter=".print">PRINT</a></li>
-            <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Contact'))); ?>">CONTACT</a></li>
-            <li><a href="<?php bloginfo('template_directory');?>/photos/SkylarResume.pdf"l target="_blank">RESUME</a></li>
-        </ul>
-        <article>&copy; Skylar Wuebker 2014</article>
-    </nav>
-
-    <!-- MOBILE NAVIGATION TRIGGER -->
-    <aside id="button" class="sidebar-small pull-left">
-            <ul class="list-unstyled filters">
-                <li><a id="menu-btn" href="#mobile-nav"><img src="<?php bloginfo('template_directory');?>/photos/button.png"></a></li>
-                <li><a class="filter" href="#identity" data-filter=".identity"><img src="<?php bloginfo('template_directory');?>/photos/identity_icon.png"></a></li>
-                <li><a class="filter" href="#web" data-filter=".web"><img src="<?php bloginfo('template_directory');?>/photos/web_icon.png"></a></li>
-                <li><a class="filter" href="#print" data-filter=".print"><img src="<?php bloginfo('template_directory');?>/photos/print_icon.png"></a></li>
-                <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Contact'))); ?>"><img src="<?php bloginfo('template_directory');?>/photos/contact_icon.png"></a></li>
-                <li><a href="<?php bloginfo('template_directory');?>/photos/SkylarResume.pdf" target="_blank"><img src="<?php bloginfo('template_directory');?>/photos/resume_icon.png"></a></li>
-            </ul>
-    </aside>
-
-    <!-- MOBILE NAVIGATION -->
-    <nav id="mobile-nav">
-        <div class="logo"><a id="menu-btn" class="filter" href="#" data-filter="*"><img src="<?php bloginfo('template_directory');?>/photos/mobile_logo.png"></div>
-        <ul class="filters">
-            <li><a class="filter" href="#identity" data-filter=".identity">IDENTITY</li></a> 
-            <li><a class="filter" href="#web" data-filter=".web">WEB</a></li>
-            <li><a class="filter" href="#print" data-filter=".print">PRINT</a></li>
-            <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Contact'))); ?>">CONTACT</a></li>
-            <li><a href="<?php bloginfo('template_directory');?>/photos/SkylarResume.pdf" target="_blank">RESUME</a></li>
-        </ul>
-        <article>&copy; Skylar Wuebker 2014</article>
-    </nav>
-
-<? } else { ?>
-
-    <nav id="nav" class="col-sm-3 hidden-xs">
-        <div class="logo"><a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_directory');?>/photos/logo.png"></a></div>
-        <ul class="filters">
-            <li><a class="filter" href="<?php bloginfo('url'); ?>" data-filter="*">HOME</a></li>
-            <li><a class="filter" href="<?php bloginfo('url'); ?>#identity" data-filter=".identity">IDENTITY</li></a> 
-            <li><a class="filter" href="<?php bloginfo('url'); ?>#web" data-filter=".web">WEB</a></li>
-            <li><a class="filter" href="<?php bloginfo('url'); ?>#print" data-filter=".print">PRINT</a></li>
-            <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Contact'))); ?>">CONTACT</a></li>
-            <li><a href="photos/SkylarResume.pdf">RESUME</a></li>
-        </ul>
-        <article>&copy; Skylar Wuebker 2014</article>
-    </nav>
-
-    <!-- MOBILE NAVIGATION TRIGGER -->
-    <aside id="button" class="sidebar-small pull-left">
-            <ul class="list-unstyled filters">
-                <li><a id="menu-btn" href="#mobile-nav"><img src="<?php bloginfo('template_directory');?>/photos/button.png"></a></li>
-                <li><a class="filter" href="<?php bloginfo('url'); ?>#identity" data-filter=".identity"><img src="<?php bloginfo('template_directory');?>/photos/identity_icon.png"></a></li>
-                <li><a class="filter" href="<?php bloginfo('url'); ?>#web" data-filter=".web"><img src="<?php bloginfo('template_directory');?>/photos/web_icon.png"></a></li>
-                <li><a class="filter" href="<?php bloginfo('url'); ?>#print" data-filter=".print"><img src="<?php bloginfo('template_directory');?>/photos/print_icon.png"></a></li>
-                <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Contact'))); ?>"><img src="<?php bloginfo('template_directory');?>/photos/contact_icon.png"></a></li>
-                <li><a href="photos/SkylarResume.pdf"><img src="<?php bloginfo('template_directory');?>/photos/resume_icon.png"></a></li>
-            </ul>
-    </aside>
-
-    <!-- MOBILE NAVIGATION -->
-    <nav id="mobile-nav">
-        <div class="logo"><a id="menu-btn" class="filter" href="<?php bloginfo('url'); ?>" data-filter="*"><img src="<?php bloginfo('template_directory');?>/photos/mobile_logo.png"></div>
-        <ul class="filters">
-            <li><a class="filter" href="<?php bloginfo('url'); ?>#identity" data-filter=".identity">IDENTITY</li></a> 
-            <li><a class="filter" href="<?php bloginfo('url'); ?>#web" data-filter=".web">WEB</a></li>
-            <li><a class="filter" href="<?php bloginfo('url'); ?>#print" data-filter=".print">PRINT</a></li>
-            <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Contact'))); ?>">CONTACT</a></li>
-            <li><a href="photos/SkylarResume.pdf">RESUME</a></li>
-        </ul>
-        <article>&copy; Skylar Wuebker 2014</article>
-    </nav>
-
+            <!-- MOBILE NAVIGATION -->
+            <nav id="mobile-nav">
+                <div class="logo"><a id="menu-btn" class="filter" href="#" data-filter="*"><img src="photos/mobile_logo.png"></div>
+                <ul class="filters">
+                    <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Work'))); ?>">WORK</a></li>
+                    <li><a href="<?php echo esc_url( get_permalink( get_page_by_title('Contact'))); ?>">CONTACT</a></li>
+                    <li><a href="<?php bloginfo('template_directory');?>/photos/SkylarResume.pdf">RESUME</a></li>
+                </ul>
+            </nav>
 <? } ?>
--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ if ($(window).width() < 768) {
 
 	$('#menu-btn').sidr({
 		name: 'sidr-main',
-		source: '#mobile-nav'
+		source: '#mobile-nav',
+		side: 'right'
 	});
-    
 };
 
 var $container = $('.items').isotope({
@@ -23,31 +23,77 @@ $('.sidr').on( 'click', 'a.sidr-class-filter', function() {
 
 $(document).ready(function() {
 	$(".fancybox")
-		.fancybox({
-			beforeLoad: function() {
-	            var el, id = $(this.element).data('title-id');
+	.fancybox({
+		beforeLoad: function() {
+			var el, id = $(this.element).data('title-id');
 
-	            if (id) {
-	                el = $('#' + id);
-	            
-	                if (el.length) {
-	                    this.title = el.html();
-	                }
-	            }
-	        },
-			closeClick : true,
-			loop : 'false',
-			openEffect : 'fade',
-			nextEffect : 'fade',
-			prevEffect : 'fade',
-			helpers : {
-				title : {
-					type : 'inside'
-				},
-				overlay : {
-					closeClick : true,
-					showEarly  : true
+			if (id) {
+				el = $('#' + id);
+
+				if (el.length) {
+					this.title = el.html();
 				}
 			}
-		});
+		},
+		closeClick : true,
+		openEffect : 'fade',
+		helpers : {
+			title : {
+				type : 'inside'
+			},
+			overlay : {
+				closeClick : true,
+				showEarly  : true
+			}
+		}
+	});
+});
+
+var a = $(".nav").offset().top;
+
+$(document).scroll(function(){
+	if($(this).scrollTop() > a)
+	{   
+		$('.nav').css({"background":"white"});
+		$('.nav').css({"box-shadow":"3px 3px 20px rgba(0,0,0,0.5)"});
+		$('.logo').css({"background-image":"url(photos/button.png)"});
+	} else {
+		$('.nav').css({"background":"transparent"});
+		$('.nav').css({"box-shadow":"3px 3px 20px rgba(0,0,0,0)"});
+		$('.logo').css({"background-image":"url(photos/button-white.png)"});
+	}
+});
+$(document).ready( function(){
+	$('.dayton').hover( 
+		//What happens when the mouse enters
+		function() {
+			$('.location').css('background-image', 'url(photos/dayton.png)');
+			$('.dayton').css('color', 'white');
+			$('.akron').css('color', 'white');
+			$('.akron').css('opacity', '.4');
+			$('.dayton').css('opacity', '1');
+		}, 
+		// What happens when the mouse leaves?
+		function() {
+			$('.location').css('background-image', 'url(photos/dayton.png)');
+			$('.dayton').css('color', '#FFF');
+			$('.akron').css('color', '#FFF');
+			$('.dayton').css('opacity', '1');
+		}
+	);
+
+	$('.akron').hover(
+		function() {   
+			$('.location').css('background-image', 'url(photos/akron.png)');
+			$('.dayton').css('color', 'white');
+			$('.dayton').css('opacity', '.4');
+			$('.akron').css('color', 'white');
+			$('.akron').css('opacity', '1');
+		}, function(){
+			$('.location').css('background-image', 'url(photos/akron.png)');
+			$('.dayton').css('color', '#FFF');
+			$('.akron').css('color', '#FFF');
+			$('.akron').css('opacity', '1');
+		}
+	);
 });
